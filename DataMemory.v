43f797bbf1c2357247196bea57cf239512a3ce75
vtta <vtta0124@gmail.com>
2019-04-13 20:12:58 +0800

    delete debug code in data memory and register files

--- DataMemory.v
+++ DataMemory.v
@@ -1,29 +1,39 @@
 
-module DataMemory(DataOut,DataAddr,DataIn,DMemW,DMemR,CLK);
-input [4:0]     DataAddr;
-input [31:0]    DataIn;
-input           DMemR;
-input           DMemW;
-input           CLK;
+module DataMemory (
+    input clk,          // Clock
+    // input clk_en,    // Clock Enable
+    input rst,          // Asynchronous reset active low
 
-output[31:0] DataOut;
+    input[4:0]  DataAddr,
+    input[31:0] DataIn,
+    input       DMemR,
+    input       DMemW,
 
-reg [31:0]  DataMemory[1023:0];
+    output[31:0] DataOut
+);
+
+
+reg [31:0] DataMemory[31:0];
+assign DataOut = DataMemory[DataAddr];
 integer i;
 
-always@(posedge CLK)
+initial begin
+    for (i = 0; i < 32; i++) begin
+        DataMemory[i] <= 0;
+    end
+end
+
+always @(posedge rst) begin
+    for (i = 0; i < 32; i++) begin
+        DataMemory[i] <= 0;
+    end
+end
+
+always@(posedge clk)
 begin
     if(DMemW) begin
-        DataMemory[DataAddr] = DataIn;
+        DataMemory[DataAddr] <= DataIn;
     end
-    
-    for(i=0;i<4;i=i+1) begin
-        $display("M[%2d-%2d]\t%8X\t%8X\t%8X\t%8X",i*4,i*4+3, DataMemory[i*4+0], DataMemory[i*4+1], DataMemory[i*4+2], DataMemory[i*4+3]);
-    end
-    if(DMemW) begin
-        $display("M[ %4X]\t%8X", DataAddr, DataIn);
-    end
-
 end
-assign DataOut = DataMemory[DataAddr];
+
 endmodule
--- GPR.v
+++ GPR.v
@@ -1,35 +1,42 @@
 
-module GPR(DataOut1,DataOut2,CLK,WriteData,RegWrite,WriteRegisterSelect,ReadRegister1,ReadRegister2);
-input CLK;
-input RegWrite;
-input [4:0] WriteRegisterSelect,ReadRegister1,ReadRegister2;
-input [31:0] WriteData;
+module GPR (
+    input clk,          // Clock
+    // input clk_en,    // Clock Enable
+    input rst,          // Asynchronous reset active low
+    
+    input[4:0]  ReadRegister1,
+    input[4:0]  ReadRegister2,
+    input       RegWrite,
+    input[4:0]  WriteRegister,
+    input[31:0] WriteData,
 
-output [31:0] DataOut1,DataOut2;
+    output[31:0] DataOut1,
+    output[31:0] DataOut2
+);
 
 reg [31:0] gprRegisters[31:0];
-integer i;
-
-initial
-begin
-    gprRegisters[0] = 0;
-end
-
-always@(posedge CLK)
-begin
-    gprRegisters[0] = 0;
-    if(RegWrite) begin
-        gprRegisters[WriteRegisterSelect] = WriteData;
-    end
-    $display("=====================================");
-    for(i=0;i<8;i=i+1) begin
-        $display("R[%2d-%2d]\t%8X\t%8X\t%8X\t%8X",i*4,i*4+3, gprRegisters[i*4+0], gprRegisters[i*4+1], gprRegisters[i*4+2], gprRegisters[i*4+3]);
-    end
-    if(RegWrite) begin
-        $display("R[ %4d]\t%8X", WriteRegisterSelect, gprRegisters[WriteRegisterSelect]);
-    end
-end
 assign DataOut1 = gprRegisters[ReadRegister1];
 assign DataOut2 = gprRegisters[ReadRegister2];
+integer i;
+
+initial begin
+    for (i = 0; i < 32; i++) begin
+        gprRegisters[i] <= 0;
+    end
+end
+
+always @(posedge rst) begin
+    for (i = 0; i < 32; i++) begin
+        gprRegisters[i] <= 0;
+    end
+end
+
+always@(posedge clk)
+begin
+    gprRegisters[0] <= 0;
+    if(RegWrite) begin
+        gprRegisters[WriteRegister] <= WriteData;
+    end
+end
 
 endmodule
